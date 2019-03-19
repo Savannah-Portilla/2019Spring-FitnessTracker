8633363d62300402860ea899c94e383820c7e45c
Savannah <savannah.l.portilla@gmail.com>
2019-03-18 21:38:34 -0400

    end of video 2 - index.js error, line 21: TypeError: Cannot read property 'name' of undefined

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const Users = require('./controllers/Users');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
